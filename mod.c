6fc9f52d50fef549378218f259e7186030607fbc
movsesyanpv <movsesyanpv1995@live.com>
2017-07-16 14:30:04 +0300

    Fixed maybe

--- mod.c
+++ mod.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <lapacke.h>
+//#include <lapacke.h>
 #include <math.h>
+#include <mkl.h>
 
 int main()
 {
-    FILE *in;
+	FILE *in;
     int i=0, j=0, l=0;
     double sum=0;
     double cov[7][7];
+	double s = 0;
     //double **a;     // матрица системы
-    double a[7][9];
+	double a[7][9], ba[7][9];
     //double m[7];    // столбец неизвестных
 /*    double *x;      // столбец свободных членов*/
     double w[7];
-    double x[9];
+    double x[9], y[9];
 /*    double *xs;     // X_s, Y_s, Z_s*/
     double xs[9];
     lapack_int n=3;        // количество точек с известными координатами
@@ -25,26 +27,20 @@ int main()
     
     k = 3 * n;
     
-/*    a = (double**) malloc(7*sizeof(double*));*/
-/*    for(int i=0; i<7; i++)*/
-/*    {*/
-/*        a[i] = (double*) malloc(k * sizeof(double));  */
-/*    }*/
-    
-/*    x = (double*) malloc(k * sizeof(double));*/
-/*    xs = (double*) malloc(k * sizeof(double));*/
-    
-    in = fopen("xxs.dat", "r");
+    info = fopen_s(&in,"E:\s.dat", "r");
     for(i = 0; i<k; i++)
     {
-        fscanf(in,"%15lf", &xs[i]);
-/*        xs[i] = i+1;*/
+        fscanf_s(in,"%15lf", &xs[i]);
+        //xs[i] = i+1;
     }
+	//xs[6] = 0;
+	//xs[2] = -10;
     
     for(i = 0; i<k; i++)
     {
-        fscanf(in,"%15lf", &x[i]);
-/*        x[i] = i+1;*/
+        fscanf_s(in,"%15lf", &x[i]);
+        //x[i] = i+1;
+		y[i] = x[i];
     }
     fclose(in);
     
@@ -61,7 +57,7 @@ int main()
         a[1][3*i+1] = xs[3*i];
         a[4][3*i+1] = -xs[3*i+2];
         a[5][3*i+1] = 1;
-        a[2][3*i+1] = a[3][3*i+1] = a[6][3*i] = 0;
+        a[2][3*i+1] = a[3][3*i+1] = a[6][3*i+1] = 0;
         
         a[0][3*i+2] = xs[3*i+2];
         a[2][3*i+2] = -xs[3*i];
@@ -75,6 +71,7 @@ int main()
             for(j = 0; j<7; j++)
                 { 
                     printf("%16lf", a[j][i]);
+					ba[j][i] = a[j][i];
                 }
             printf(" xs=%15lf x=%15lf\n",xs[i], x[i]);
         }
@@ -87,14 +84,12 @@ int main()
         {   
             for(j = 0; j<7; j++)
                 { 
-                    printf("%16lf", a[j][i]);
+                    printf("%20.10e", a[j][i]);
                 }
 /*            printf(" xs=%15lf x=%15lf\n",xs[i], x[i]);*/
                 printf("\n");
         }
         printf("\n");
-    
-    printf("m =%.10e\nRx=%lf\nRy=%lf\nRz=%lf\ndX=%lf\ndY=%lf\ndZ=%lf\n", x[0]-1, x[4]/x[0], x[2]/x[0], x[1]/x[0], x[3], x[5], x[6]);
     
         for(j = 0; j<7; j++)
         {
@@ -103,9 +98,9 @@ int main()
                 sum = 0;   
                 for(i = 0; i<7; i++)
                 {
-                    if(w[i]>1.0E-8)
+                    if(abs(w[i])>1.0E-16)
                     {
-                        sum += a[i][j]*a[i][k]/w[i]/w[i];
+                        sum += a[i][j]*a[i][l]/w[i]/w[i];
                     }
                 }
                 cov[j][l] = sum;
@@ -122,6 +117,24 @@ int main()
                 printf(" w=%lf\n",w[i]);
         }
         printf("\n");
+
+		for (i = 0; i < 9; i++)
+		{
+			sum = 0;
+			for (j = 0; j < 7; j++)
+			{
+				sum += ba[j][i] * x[j];
+			}
+			s += pow((y[i] - sum), 2);
+		}
+		s = sqrt(s / (9.0 - 7.0));
+
+		for (i = 0; i < 7; i++)
+		{
+			cov[i][i] = sqrt(cov[i][i])*s;
+		}
+
+		printf("m =%18.10e +- %.10e\nRx=%18lf +- %.10e\nRy=%18lf +- %.10e\nRz=%18lf +- %.10e\ndX=%18lf +- %.10e\ndY=%18lf +- %.10e\ndZ=%18lf +- %.10e\n", x[0] - 1, cov[0][0], x[4] / x[0], cov[1][4], x[2] / x[0], cov[2][2], x[1] / x[0],cov[1][1], x[3],cov[3][3], x[5],cov[5][5], x[6],cov[6][6]);
     
 /*    printf("ready to free\n");*/
     
